2e9907428fe9120a2fae4226f0ab9b20a2ac5cd1
jarrodcacree <jarrod@thisismyreal.email>
2026-07-05 13:49:03 -0400

    update

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from contacts.models import User
-
+from contacts.models import User, Contact
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Contact)
--- contacts/models.py
+++ contacts/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='contacts')  # related_name allows querying like users.contacts.all()
+
+    class Meta:
+        unique_together = ('user', 'email')  # adds a constraint to ensure uniqueness of email per user
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
