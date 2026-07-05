64f95a26767464699580f5fd4b66f57ee483c72a
jarrodacree <jarrodcacree@gmail.com>
2026-07-05 15:49:26 -0400

    update

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 
 # Create your views here.
 @login_required
@@ -7,3 +9,15 @@ def index(request):
     contacts = request.user.contacts.all().order_by('-created_at')
     context = {'contacts': contacts}
     return render(request, 'contacts.html', context)
+
+
+@login_required
+def search_contacts(request):
+    query = request.GET.get('search', '')
+    # filter on name or email
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    ).order_by('-created_at')
+    context = {'contacts': contacts}
+
+    return render(request, 'partials/contact-list.html', {"contacts": contacts})
