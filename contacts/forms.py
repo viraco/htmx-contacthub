a1e20fe76d21ef7c5c9d054521f332707e1add88
jarrodacree <jarrodcacree@gmail.com>
2026-07-05 21:05:50 -0400

    update

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,1 +1,22 @@
 from django import forms
+
+from contacts.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(
+                            attrs={'class': 'input input-bordered w-full',
+                                   'placeholder': 'Contact Name'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+                            attrs={'class': 'input input-bordered w-full',
+                                   'placeholder': 'Contact Email'}))
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Contact.objects.filter(user = self.initial.get('user'), email=email).exists():
+            raise forms.ValidationError('This email is already in use.')
+        return email
+
+    class Meta:
+        model = Contact
+        fields = ['name', 'email']
--- contacts/views.py
+++ contacts/views.py
@@ -1,13 +1,18 @@
+from urllib import response
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from django.db.models import Q
+from contacts.forms import ContactForm
 
 
 # Create your views here.
 @login_required
 def index(request):
     contacts = request.user.contacts.all().order_by('-created_at')
-    context = {'contacts': contacts}
+    context = {'contacts': contacts,
+               'form': ContactForm()}
     return render(request, 'contacts.html', context)
 
 
@@ -20,4 +25,27 @@ def search_contacts(request):
     ).order_by('-created_at')
     context = {'contacts': contacts}
 
-    return render(request, 'partials/contact-list.html', {"contacts": contacts})
+    response = render(request, 'partials/contact-list.html', {"contacts": contacts})
+    return response
+
+
+@login_required
+@require_http_methods(['POST'])
+def create_contact(request):
+    form = ContactForm(request.POST, initial={'user': request.user})
+    if form.is_valid():
+        contact = form.save(commit=False)
+        contact.user = request.user
+        contact.save()
+        context = {
+            "contact": contact
+        }
+        response = render(request, 'partials/contact-row.html', context)
+        response['HX-Trigger'] = 'contact-create-success'
+        return response
+    else:
+        response = render(request, 'partials/add-contact-modal.html', {'form': form})
+        response['HX-Retarget'] = '#contact_modal'
+        response['HX-ReSwap'] = 'outerHTML'
+        response['HX-Trigger-After-Settle'] = 'contact-create-error'
+        return response
